fc9773ad783cce8686076b984dd0f9e87c8681bb
Juan Camilo Soto Pineda <juancamilo.soto@outlook.com>
2025-06-04 23:32:57 -0500

    Update root route to return API response for web access
    - Changed the root route to return a JSON response indicating that the project is in API mode only.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Enums\HttpStatusCodes;
+use App\Services\ApiResponse;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return ApiResponse::sendResponse(message: __('Project is API mode only'), httpCode: HttpStatusCodes::FORBIDDEN_403);
 });
